28ce8a0f295fa048a73aef50913857c59df657db
khm0813 <khm0813@modernwave.co.kr>
2020-06-08 10:11:10 +0900

    init

---
+++ SOURCE/src/test/java/kr/co/commin/spring_web/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.commin.spring_web.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
